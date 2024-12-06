37d6042fe414c481ceefbe002bb21dad03dfd367
ica <icapinto90@gmail.com>
2024-12-06 19:11:18 +0100

    use class logic on app and scene

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,19 @@
-import * as PIXI from "pixi.js";
-const app = new PIXI.Application();
-await app.init({ resizeTo: window, backgroundColor: 0x1099bb });
-document.body.appendChild(app.canvas);
+import { Application } from 'pixi.js';
+import { Scene } from './scene';
 
-export default app;
+class GameApplication {
+  constructor() {
+    this.app = new Application();
+    this.initApp();
+  }
+
+  async initApp() {
+    await this.app.init({ resizeTo: window });
+    document.body.appendChild(this.app.canvas);
+    this.scenes = new Scene();
+    this.app.stage.interactive = true;
+    this.app.stage.addChild(this.scenes.container);
+  }
+}
+
+export const App = new GameApplication();
--- src/scripts/scene.js
+++ src/scripts/scene.js
@@ -1,24 +1,34 @@
 import * as PIXI from 'pixi.js';
+import { App } from './app.js';
 
-import app from './app.js';
+export class Scene {
+  constructor() {
+    this.container = new PIXI.Container();
+    this.container.interactive = true;
+    this.scene = null;
+    this.showScene();
+  }
 
-const container = new PIXI.Container();
-app.stage.addChild(container);
-
-//ajout du background
-const background = await PIXI.Assets.load('src/Assets/Background/PNG/Postapocalypce1/Bright/clouds1.png')
-const nuages = new PIXI.Sprite(background);
-nuages.width = app.renderer.width;
-nuages.height = app.renderer.height;
-nuages.x = 0;
-nuages.y = 0;
-container.addChild(nuages);
-//ajout de la route
-const roadSprite = await PIXI.Assets.load('src/Assets/Background/PNG/Postapocalypce1/Bright/road.png');
-const road = new PIXI.Sprite(roadSprite);
-road.width = app.renderer.width;
-road.height = app.renderer.height;
-road.x = 0;
-road.y = 0;
-container.addChild(road);
-
+  async showScene() {
+    //ajout du background
+    const background = await PIXI.Assets.load(
+      'src/Assets/Background/PNG/Postapocalypce1/Bright/clouds1.png'
+    );
+    const nuages = new PIXI.Sprite(background);
+    nuages.width = App.app.renderer.width;
+    nuages.height = App.app.renderer.height;
+    nuages.x = 0;
+    nuages.y = 0;
+    this.container.addChild(nuages);
+    //ajout de la route
+    const roadSprite = await PIXI.Assets.load(
+      'src/Assets/Background/PNG/Postapocalypce1/Bright/road.png'
+    );
+    const road = new PIXI.Sprite(roadSprite);
+    road.width = App.app.renderer.width;
+    road.height = App.app.renderer.height;
+    road.x = 0;
+    road.y = 0;
+    this.container.addChild(road);
+  }
+}
