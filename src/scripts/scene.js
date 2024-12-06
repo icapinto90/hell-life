c0141b31514cd85c9866786e070a7e3cfda61d25
Jaquier Arthur~ <arthur.jaquier@rpn.ch>
2024-12-06 15:57:20 +0100

    add charcater png

--- src/scripts/scene.js
+++ src/scripts/scene.js
@@ -5,6 +5,7 @@ import app from './app.js';
 const container = new PIXI.Container();
 app.stage.addChild(container);
 
+//ajout du background
 const background = await PIXI.Assets.load('src/Assets/Background/PNG/Postapocalypce1/Bright/clouds1.png')
 const nuages = new PIXI.Sprite(background);
 nuages.width = app.renderer.width;
@@ -12,7 +13,7 @@ nuages.height = app.renderer.height;
 nuages.x = 0;
 nuages.y = 0;
 container.addChild(nuages);
-
+//ajout de la route
 const roadSprite = await PIXI.Assets.load('src/Assets/Background/PNG/Postapocalypce1/Bright/road.png');
 const road = new PIXI.Sprite(roadSprite);
 road.width = app.renderer.width;
@@ -20,3 +21,4 @@ road.height = app.renderer.height;
 road.x = 0;
 road.y = 0;
 container.addChild(road);
+
