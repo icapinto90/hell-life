69b63cade59fba9f5ecfe997eba50ed9944417c2
Jaquier Arthur~ <arthur.jaquier@rpn.ch>
2024-12-06 15:33:18 +0100

    background and road add

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,4 +2,5 @@ import * as PIXI from "pixi.js";
 const app = new PIXI.Application();
 await app.init({ resizeTo: window, backgroundColor: 0x1099bb });
 document.body.appendChild(app.canvas);
+
 export default app;
---
+++ src/scripts/scene.js
@@ -0,0 +1,22 @@
+import * as PIXI from 'pixi.js';
+
+import app from './app.js';
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+const background = await PIXI.Assets.load('src/Assets/Background/PNG/Postapocalypce1/Bright/clouds1.png')
+const nuages = new PIXI.Sprite(background);
+nuages.width = app.renderer.width;
+nuages.height = app.renderer.height;
+nuages.x = 0;
+nuages.y = 0;
+container.addChild(nuages);
+
+const roadSprite = await PIXI.Assets.load('src/Assets/Background/PNG/Postapocalypce1/Bright/road.png');
+const road = new PIXI.Sprite(roadSprite);
+road.width = app.renderer.width;
+road.height = app.renderer.height;
+road.x = 0;
+road.y = 0;
+container.addChild(road);
