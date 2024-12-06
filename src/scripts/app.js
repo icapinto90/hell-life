faf341c49220b59114c099e75787efca789faed8
icapinto90 <icapinto90@gmail.com>
2024-12-06 14:54:14 +0100

    init project v2.0

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,5 @@
-let app = new PIXI.Application({
-  resizeTo: window,
-  backgroundColor: 0x1099bb,
-});
-
-document.body.appendChild(app.view);
-
+import * as PIXI from "pixi.js";
+const app = new PIXI.Application();
+await app.init({ resizeTo: window, backgroundColor: 0x1099bb });
+document.body.appendChild(app.canvas);
 export default app;
