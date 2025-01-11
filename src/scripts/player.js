822fe68b2afbffa1ccce0a4fa88843b5e7cfae06
ica <icapinto90@gmail.com>
2025-01-11 17:11:54 +0100

    Fix player and zombie path

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,27 +1,27 @@
-import * as PIXI from "pixi.js";
-import { App } from "./app.js";
-import { Scene } from "./scene.js";
-import { groupD8 } from "pixi.js";
-import { getGroundYAtX } from "./utils/getGroundYAtX.js";
+import * as PIXI from 'pixi.js';
+import { App } from './app.js';
+import { Scene } from './scene.js';
+import { groupD8 } from 'pixi.js';
+import { getGroundYAtX } from './utils/getGroundYAtX.js';
 
 export class Player {
   constructor() {
     this.container = new PIXI.Container();
     this.container.interactive = true;
-            this.speed = 5;
-            this.jumpSpeed = 10;
-            this.gravity = 0.5;
-            this.isJumping = false;
-            this.isFalling = false;
-            this.velocityY = 0;
-            this.groundY = 377;
-            this.keys = {};
-            this.animations = {};
-            this.currentState = 'idle';
-            this.movingLeft = false;
-            this.movingRight = false;
-            this.health = 100;
-            this.maxHealth = 100;
+    this.speed = 5;
+    this.jumpSpeed = 10;
+    this.gravity = 0.5;
+    this.isJumping = false;
+    this.isFalling = false;
+    this.velocityY = 0;
+    this.groundY = 377;
+    this.keys = {};
+    this.animations = {};
+    this.currentState = 'idle';
+    this.movingLeft = false;
+    this.movingRight = false;
+    this.health = 100;
+    this.maxHealth = 100;
     this.character = null;
     this.setUpControls();
     (async () => {
@@ -31,28 +31,37 @@ export class Player {
 
   async chargePlayer() {
     this.animations.idle = await this.loadFrame(
-      "src/Assets/Character/Hero/PNG/PNG Sequences/Idle/0_Fallen_Angels_Idle_",
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Idle/0_Fallen_Angels_Idle_',
       18
     );
     this.animations.walking = await this.loadFrame(
-      "src/Assets/Character/Hero/PNG/PNG Sequences/Walking/0_Fallen_Angels_Walking_",
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Walking/0_Fallen_Angels_Walking_',
       24
     );
     this.animations.running = await this.loadFrame(
-      "src/Assets/Character/Hero/PNG/PNG Sequences/Running/0_Fallen_Angels_Running_",
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Running/0_Fallen_Angels_Running_',
       12
     );
     this.animations.jumping = await this.loadFrame(
-      "src/Assets/Character/Hero/PNG/PNG Sequences/Jump Start/0_Fallen_Angels_Jump Start_",
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Jump Start/0_Fallen_Angels_Jump Start_',
       6
     );
     this.animations.fallingDown = await this.loadFrame(
-      "src/Assets/Character/Hero/PNG/PNG Sequences/Falling Down/0_Fallen_Angels_Falling Down_",
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Falling Down/0_Fallen_Angels_Falling Down_',
       6
     );
-    this.animations.attackingIdle = await this.loadFrame("src/Assets/Character/Hero/PNG/PNG Sequences/Slashing/0_Fallen_Angels_Slashing_", 12);
-    this.animations.attackingRunning = await this.loadFrame("src/Assets/Character/Hero/PNG/PNG Sequences/Run Slashing/0_Fallen_Angels_Run Slashing_", 12);
-    this.animations.attackingInTheAir = await this.loadFrame("src/Assets/Character/Hero/PNG/PNG Sequences/Slashing in The Air/0_Fallen_Angels_Slashing in The Air_", 12);
+    this.animations.attackingIdle = await this.loadFrame(
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Slashing/0_Fallen_Angels_Slashing_',
+      12
+    );
+    this.animations.attackingRunning = await this.loadFrame(
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Run Slashing/0_Fallen_Angels_Run Slashing_',
+      12
+    );
+    this.animations.attackingInTheAir = await this.loadFrame(
+      'src/Assets/Character/Hero/PNG/PNG Sequences/Slashing in The Air/0_Fallen_Angels_Slashing in The Air_',
+      12
+    );
 
     this.character = new PIXI.AnimatedSprite(this.animations.idle);
     this.character.width = 150;
@@ -70,14 +79,13 @@ export class Player {
     // Position par rapport au personnage
     this.healthBar.position.x = this.character.x;
     this.healthBar.position.y = this.character.y - 100; // Au-dessus du personnage
-    
+
     this.container.addChild(this.character);
     this.container.addChild(this.healthBar);
-    
+
     this.updateHealthBar(); // Mise à jour initiale
 
     this.container.addChild(this.character);
-
   }
   updateHealthBar() {
     const barWidth = 250;
@@ -92,16 +100,15 @@ export class Player {
 
     // Fond de la barre (gris)
     this.healthBar
-        .rect(-barWidth / 2, 0, barWidth, barHeight) // Définir le rectangle
-        .fill(0x333333); // Appliquer le remplissage gris
+      .rect(-barWidth / 2, 0, barWidth, barHeight) // Définir le rectangle
+      .fill(0x333333); // Appliquer le remplissage gris
 
     // Barre de santé (rouge ou vert selon le pourcentage)
     const fillColor = healthPercentage > 0.5 ? 0x00ff00 : 0xff0000;
     this.healthBar
-        .rect(-barWidth / 2, 0, barWidth * healthPercentage, barHeight) // Définir la taille en fonction du pourcentage
-        .fill(fillColor); // Appliquer le remplissage
-}
-
+      .rect(-barWidth / 2, 0, barWidth * healthPercentage, barHeight) // Définir la taille en fonction du pourcentage
+      .fill(fillColor); // Appliquer le remplissage
+  }
 
   async loadFrame(basePath, frameCount) {
     const frames = [];
@@ -116,149 +123,147 @@ export class Player {
     return frames;
   }
 
-
-
   setAnimationSpeed(speed) {
     if (this.character) {
-        this.character.animationSpeed = speed;
-    }
-}
-
-setUpControls() {
-  window.addEventListener("keydown", (event) => {
-      this.keys[event.key] = true;
-
-      if ((event.key === "ArrowUp" || event.key === "w") && !this.isJumping) {
-          this.isJumping = true;
-          this.velocityY = -this.jumpSpeed;
-          if (!this.isAttacking) {
-              this.setAnimation("jumping");
-              this.setAnimationSpeed(0.2); // Vitesse par défaut pour le saut
-          }
-      }
-
-      // Détection de l'attaque (barre d'espace)
-      if (event.key === " " && !this.isAttacking) {
-          this.isAttacking = true;
-
-          // Augmenter la vitesse d'animation pour les attaques
-          this.setAnimationSpeed(0.3); // Ajustez cette valeur pour accélérer les attaques
-
-          // Sélection de l'animation d'attaque en fonction de l'état
-          if (this.isJumping) {
-              this.setAnimation("attackingInTheAir");
-          } else if (this.movingLeft || this.movingRight) {
-              this.setAnimation("attackingRunning");
-          } else {
-              this.setAnimation("attackingIdle");
-          }
-
-          // Temporisation pour désactiver l'attaque
-          setTimeout(() => {
-              this.isAttacking = false;
-
-              // Retour à la vitesse normale et à l'animation appropriée
-              this.setAnimationSpeed(0.2); // Réinitialisez à la vitesse normale
-              if (this.isJumping) {
-                  this.setAnimation("jumping");
-              } else if (this.movingLeft || this.movingRight) {
-                  this.setAnimation("running");
-              } else {
-                  this.setAnimation("idle");
-              }
-          }, 500); // Durée de l'animation d'attaque
-      }
-  });
-
-  window.addEventListener("keyup", (event) => {
-      this.keys[event.key] = false;
-  });
-}
-
-
-  setAnimation(state) {
-    if (this.currentState !== state) {
-        this.currentState = state;
-        this.character.textures = this.animations[state];
-        this.character.gotoAndPlay(0); // Redémarre l'animation
+      this.character.animationSpeed = speed;
     }
   }
 
+  setUpControls() {
+    window.addEventListener('keydown', (event) => {
+      this.keys[event.key] = true;
 
-
-  update() {
-    if (this.character) {
-        // Déplacement horizontal
-        if (this.keys['ArrowLeft'] || this.keys['a']) {
-            this.character.x -= this.speed;
-
-            if (this.character.scale.x > 0) {
-                this.character.scale.x *= -1;
-            }
-
-            this.movingLeft = true;
-            this.movingRight = false;
-
-            // Appliquer la bonne animation
-            if (this.isAttacking) {
-                this.setAnimation('attackingRunning');
-            } else if (!this.isJumping) {
-                this.setAnimation('running');
-            }
-        } else if (this.keys['ArrowRight'] || this.keys['d']) {
-            this.character.x += this.speed;
-
-            if (this.character.scale.x < 0) {
-                this.character.scale.x *= -1;
-            }
-
-            this.movingRight = true;
-            this.movingLeft = false;
-
-            // Appliquer la bonne animation
-            if (this.isAttacking) {
-                this.setAnimation('attackingRunning');
-            } else if (!this.isJumping) {
-                this.setAnimation('running');
-            }
-        } else {
-            // Pas de mouvement horizontal
-            this.movingLeft = false;
-            this.movingRight = false;
-
-            if (!this.isJumping && !this.isAttacking && this.currentState !== 'idle') {
-                this.setAnimation('idle');
-            }
+      if ((event.key === 'ArrowUp' || event.key === 'w') && !this.isJumping) {
+        this.isJumping = true;
+        this.velocityY = -this.jumpSpeed;
+        if (!this.isAttacking) {
+          this.setAnimation('jumping');
+          this.setAnimationSpeed(0.2); // Vitesse par défaut pour le saut
         }
+      }
 
-        // Gestion du saut
+      // Détection de l'attaque (barre d'espace)
+      if (event.key === ' ' && !this.isAttacking) {
+        this.isAttacking = true;
+
+        // Augmenter la vitesse d'animation pour les attaques
+        this.setAnimationSpeed(0.3); // Ajustez cette valeur pour accélérer les attaques
+
+        // Sélection de l'animation d'attaque en fonction de l'état
         if (this.isJumping) {
-            this.character.y += this.velocityY; // Appliquer la gravité
-            this.velocityY += this.gravity; // Ajouter l'accélération due à la gravité
-
-            if (this.velocityY > 0 && !this.isFalling) {
-                this.isFalling = true;
-                if (!this.isAttacking) this.setAnimation("fallingDown");
-            }
-
-            if (this.character.y >= this.groundY) {
-                this.character.y = this.groundY;
-                this.isJumping = false;
-                this.isFalling = false;
-
-                if (!this.isAttacking) {
-                    this.setAnimation("idle");
-                }
-            }
-            this.updateHealthBar();
-            
+          this.setAnimation('attackingInTheAir');
+        } else if (this.movingLeft || this.movingRight) {
+          this.setAnimation('attackingRunning');
+        } else {
+          this.setAnimation('attackingIdle');
         }
+
+        // Temporisation pour désactiver l'attaque
+        setTimeout(() => {
+          this.isAttacking = false;
+
+          // Retour à la vitesse normale et à l'animation appropriée
+          this.setAnimationSpeed(0.2); // Réinitialisez à la vitesse normale
+          if (this.isJumping) {
+            this.setAnimation('jumping');
+          } else if (this.movingLeft || this.movingRight) {
+            this.setAnimation('running');
+          } else {
+            this.setAnimation('idle');
+          }
+        }, 500); // Durée de l'animation d'attaque
+      }
+    });
+
+    window.addEventListener('keyup', (event) => {
+      this.keys[event.key] = false;
+    });
+  }
+
+  setAnimation(state) {
+    if (this.currentState !== state) {
+      this.currentState = state;
+      this.character.textures = this.animations[state];
+      this.character.gotoAndPlay(0); // Redémarre l'animation
     }
+  }
 
-}
+  update(backgroundGround) {
+    if (this.character) {
+      // Déplacement horizontal
+      if (this.keys['ArrowLeft'] || this.keys['a']) {
+        this.character.x -= this.speed;
 
- 
+        if (this.character.scale.x > 0) {
+          this.character.scale.x *= -1;
         }
-        
-   
- 
+
+        this.movingLeft = true;
+        this.movingRight = false;
+
+        // Appliquer la bonne animation
+        if (this.isAttacking) {
+          this.setAnimation('attackingRunning');
+        } else if (!this.isJumping) {
+          this.setAnimation('running');
+        }
+      } else if (this.keys['ArrowRight'] || this.keys['d']) {
+        this.character.x += this.speed;
+
+        if (this.character.scale.x < 0) {
+          this.character.scale.x *= -1;
+        }
+
+        this.movingRight = true;
+        this.movingLeft = false;
+
+        // Appliquer la bonne animation
+        if (this.isAttacking) {
+          this.setAnimation('attackingRunning');
+        } else if (!this.isJumping) {
+          this.setAnimation('running');
+        }
+      } else {
+        // Pas de mouvement horizontal
+        this.movingLeft = false;
+        this.movingRight = false;
+
+        if (
+          !this.isJumping &&
+          !this.isAttacking &&
+          this.currentState !== 'idle'
+        ) {
+          this.setAnimation('idle');
+        }
+      }
+
+      // Gestion du saut
+      if (this.isJumping) {
+        this.character.y += this.velocityY; // Appliquer la gravité
+        this.velocityY += this.gravity; // Ajouter l'accélération due à la gravité
+
+        if (this.velocityY > 0 && !this.isFalling) {
+          this.isFalling = true;
+          if (!this.isAttacking) this.setAnimation('fallingDown');
+        }
+
+        if (this.character.y >= this.groundY) {
+          this.character.y = this.groundY;
+          this.isJumping = false;
+          this.isFalling = false;
+
+          if (!this.isAttacking) {
+            this.setAnimation('idle');
+          }
+        }
+        this.updateHealthBar();
+      } else {
+        this.groundY =
+          getGroundYAtX(backgroundGround, this.character.x) -
+          this.character.height +
+          66;
+        this.character.y = this.groundY;
+      }
+    }
+  }
+}
